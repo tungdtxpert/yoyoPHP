8e80c5923e1dcfcb21ddf3c37271db83d08da70a
Tung Dang Thanh <tungdt@5xpert.com>
2016-11-23 21:04:47 +0700

    Init Project Yoyo CMS

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+   <head>
+	<title>Yoyo Framework</title>
+	<style>
+            h3{
+		color:red;
+                font-size:23px;
+                text-align:center;
+	    }
+	</style>
+   </head>	
+   <body>
+     <h3>This is Yoyo CMS</h3>
+  </body>
+</html>
